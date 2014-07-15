fb904b54a795abec0fb260aa83472a7f3c84be0d
Chris Finlayson <chrisfinlayson@gmail.com>
2014-07-15 11:00:58 -0400

    Upload version 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adius::Application.config.secret_key_base = '7f0fa0065479bb45e22d7bad6230bef3e349196ef699e833bec3b41592cbe75cf0353a7f18e696f359c4b05ab7a41a76f273bbf87e50f91702b594f5d691eb37'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Adius::Application.config.session_store :cookie_store, key: '_adius_session'
